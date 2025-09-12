45dfdbc84850bdecb01a4d40922276cc629c9847
Pablo Costa <sottti@gmail.com>
2025-09-13 01:27:46 +0200

    Fix initial formatted time

--- app/src/main/kotlin/com/sotti/milliscope/data/MainActivityInitialState.kt
+++ app/src/main/kotlin/com/sotti/milliscope/data/MainActivityInitialState.kt
@@ -9,7 +9,7 @@ private val titleResId = R.string.app_name
 private val initialList =
     List(138) { index ->
         MainActivityItemUi(
-            formattedVisibleTimeInSeconds = "0",
+            formattedVisibleTimeInSeconds = "0.0",
             id = ItemId(index + 1),
             label = "Item ${index + 1}",
             visibleTimeInMilliSeconds = 0L,
